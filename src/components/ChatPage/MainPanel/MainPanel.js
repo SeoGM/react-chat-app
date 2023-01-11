e07649d8768b473ea883fa2e769fdff9f8b88701
bbdev <auddlzz000@gmail.com>
2023-01-11 17:05:39 +0000

    send messages and load messages

--- src/components/ChatPage/MainPanel/MainPanel.js
+++ src/components/ChatPage/MainPanel/MainPanel.js
@@ -2,9 +2,50 @@ import React, { Component } from 'react';
 import MessageHeader from './MessageHeader';
 import Message from './Message';
 import MessageForm from './MessageForm';
+import { connect } from 'react-redux';
+import { getDatabase, ref, onChildAdded, child } from "firebase/database";
 
 export class MainPanel extends Component {
+	
+	state = {
+		messages: [],
+		messagesRef: ref(getDatabase(), 'messages'),
+		messagesLoading: true,
+	}
+	
+	componentDidMount() {
+		const { chatRoom } = this.props;
+		if(chatRoom) {
+			this.addMessagesListeners(chatRoom.id);
+		}
+	}
+	
+	addMessagesListeners = (chatRoomId) => {
+		let messagesArray = [];
+		let { messagesRef } = this.state;
+		
+		onChildAdded(child(messagesRef, chatRoomId), DataSnapShot => {
+			messagesArray.push(DataSnapShot.val());
+			this.setState({
+				messages: messagesArray,
+				messagesLoading: false,
+			})
+		})
+	}
+	
+	renderMessages = (messages) => 
+		messages.length > 0 &&
+		messages.map(message => (
+			<Message 
+				key={message.timestamp}
+				message={message}
+				user={this.props.user}
+			/>
+		))
+	
 	render() {
+		const { messages } = this.state;
+		
 		return (
 			<div style={{ padding: '2rem 2rem 0 2rem' }}>
 				<MessageHeader />
@@ -18,7 +59,7 @@ export class MainPanel extends Component {
 					marginBottom: '1rem',
 					overflowY: 'auto'
 				}}>
-					
+					{ this.renderMessages(messages) }
 				</div>
 				
 				<MessageForm />
@@ -27,4 +68,11 @@ export class MainPanel extends Component {
 	}
 }
 
-export default MainPanel
+const mapStateToProps = state => {
+	return {
+		user: state.user.currentUser,
+		chatRoom: state.chatRoom.currentChatRoom
+	}
+}
+
+export default connect(mapStateToProps)(MainPanel)
--- src/components/ChatPage/MainPanel/Message.js
+++ src/components/ChatPage/MainPanel/Message.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import moment from 'moment';
 
-function Message() {
+function Message({message, user}) {
+	
+	const timeFromNow = timestamp => moment(timestamp).fromNow();
+	
+	const isImage = message => {
+		return message.hasOwnProperty("image") && !message.hasOwnProperty("content");
+	}
+	
+	const isMessageMine = (message, user) => {
+		return message.user.id === user.uid
+	}
+		
 	return (
-		<div>
-			Message
-		</div>
+		<div style={{ marginBottom: '3px', display:'flex' }}>
+			<img
+				style={{ borderRadius: '10px', marginRight: '15px' }}
+				width={48}
+				height={48}
+				src={message.user.image}
+				alt={message.user.name}
+			/>
+				<div style={{ width: '100%', backgroundColor: isMessageMine(message, user) && "#ECECEC"}}>
+					<h6>
+						{message.user.name} {" "}
+						<span style={{ fontSize: '10px', color: 'gray' }}>{timeFromNow(message.timestamp)}</span>
+					</h6>
+					{isImage(message) ?
+						<img style={{ maxWidth: '300px', }} alt="image" src={message.image} />
+						: <p>{message.content}</p>
+					}
+					
+				</div>
+			</div>
 	)
 }
 
